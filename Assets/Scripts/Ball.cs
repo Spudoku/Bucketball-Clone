da0295b1d7fb36b771e475f19ff4536cf6a5d626
Spudoku <calebisntshort@gmail.com>
2025-03-31 10:57:29 -0700

    hi

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// click and drag to aim
+// interacts with an Aimer to help the player
+public class Ball : MonoBehaviour
+{
+    [SerializeField] float sensitivity;         // how much the power scales per unit the mouse is dragged
+
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
